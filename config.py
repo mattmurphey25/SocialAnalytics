8a74a78179e1cf23c608c01700d83c51b95dc050
mattmurphey25 <mattmurphey25@yahoo.com>
2018-07-11 20:40:58 -0500

    update

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 # Twitter API Keys
+consumer_key = "0HTaFgxWxcdjHKKFkDiCfNtcK"
+consumer_secret = "zFhLQTne8Xu1rBBdPkLcThkjofWfocI0zluKEUuqhopYb6myeB"
+access_token = "564357465-v7JTqbjEyOeFiegpT92KefsjCexXAJD6OnSbCJKh"
+access_token_secret = "wB3eJmxMUlSonwQeq3GBf0aUmzwCSdrIVysENa2Hr0fwC"
+
+weather_api_key="12f67a0810aab7ad7d238f94f4ccd1d3"
